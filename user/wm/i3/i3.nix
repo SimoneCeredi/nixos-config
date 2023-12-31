adf95de5175e408086094f57245243b738c2cde8
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-12-31 16:17:53 +0100

    feat: added i3 configuration

--- user/wm/i3/i3.nix
+++ user/wm/i3/i3.nix
@@ -23,9 +23,30 @@ in {
           names = [ "JetBrains Mono" ];
           size = 9.0;
         };
-      statusCommand = "i3status-rs $HOME/.config/i3status-rust/config-top.toml";
-      extraConfig = "height 16";
-    }];
+        statusCommand = "i3status-rs $HOME/.config/i3status-rust/config-top.toml";
+        extraConfig = "height 16";
+      }];
+
+      keybindings = lib.mkOptionDefault {
+        "${mod}+p" = "exec ${pkgs.dmenu-rs}/bin/dmenu-rs";
+        "${mod}+x" = "exec sh -c '${pkgs.maim}/bin/maim -s | xclip -selection clipboard -t image/png'";
+        "${mod}+Escape" = "exec sh -c '${pkgs.i3lock}/bin/i3lock -c 222222 & sleep 5 && xset dpms force of'";
+
+        # Focus
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+
+        # Move
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+
+        # kill app
+        "${mod}+q" = "kill";
+      };
     };
   };
 }
